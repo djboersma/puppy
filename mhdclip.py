3deb30f1a5511a4294e5c9ce3dd38fbe470586cf
David Boersma <David>
2016-06-23 20:14:48 +0200

    no hardcoded paths

--- mhdclip.py
+++ mhdclip.py
@@ -6,7 +6,7 @@ from glob import glob
 
 
 mhdlist=[]
-zapdir='/Users/montecarlo/Analysis/DICOM/Hodgkin/zaptestdata'
+zapdir=os.environ.get('PATIENT_DATA_COLLECTION',None)
 for a in sys.argv[1:]:
     print "checking a=",a
     nglob=0
@@ -18,13 +18,14 @@ for a in sys.argv[1:]:
             mhdlist.append(b)
     if nglob>0:
         continue
-    print "checking zapdir/a=",os.path.join(zapdir,a)
-    for b in glob(os.path.join(zapdir,a)):
-        print "checking b=",b
-        if b[-4:]==".mhd":
-            print "bingo"
-            nglob+=1
-            mhdlist.append(b)
+    if zapdir:
+        print "checking zapdir/a=",os.path.join(zapdir,a)
+        for b in glob(os.path.join(zapdir,a)):
+            print "checking b=",b
+            if b[-4:]==".mhd":
+                print "bingo"
+                nglob+=1
+                mhdlist.append(b)
 
 print mhdlist
 #sys.exit(0)
