1bbfa1ad571fd2ad82ac533594a4be6d3961217c
David Boersma <David>
2016-08-18 13:11:22 +0200

    using SimpleITK's own Clamp function if SimpleITK is new enough

--- mhdclip.py
+++ mhdclip.py
@@ -31,8 +31,23 @@ print mhdlist
 #sys.exit(0)
 
 import SimpleITK as sitk
-for mhd in mhdlist:
-    img=sitk.ReadImage(mhd)
+
+def new_sitk_clamp(img):
+    """
+    Clamping (forcing pixel values within a range) can be done with
+    sitk.Clamp(img,pixelvalue,lower,upper), but in SimpleITK version 0.8.* this
+    function was buggy (output image always identical to input image).
+    This wrapper function is used in case your SimpleITK installation seems new enough.
+    """
+    return sitk.Clamp(img,img.GetPixelIDValue(),-1024.,3000.)
+
+def old_sitk_clamp(img):
+    """
+    Clamping (forcing pixel values within a range) can be done with
+    sitk.Clamp(img,pixelvalue,lower,upper), but in SimpleITK version 0.8.* this
+    function was buggy (output image always identical to input image).
+    This wrapper function is used in case your SimpleITK installation seems too old.
+    """
     aimg=sitk.GetArrayFromImage(img)
     nz,ny,nx=aimg.shape
     mx,my,mz=img.GetSize()
@@ -40,14 +55,31 @@ for mhd in mhdlist:
     print("mx={} my={} mz={}".format(mx,my,mz))
     assert( (mx,my,mz) == (nx,ny,nz) )
     mask=(aimg>3000)
+    nover=np.sum(mask.flat)
+    print("{} pixels have a value larger than 3000".format(nover))
     zi,yi,xi = np.indices((nz,ny,nx))
     for iz,iy,ix,pix0 in zip(zi[mask].flat,yi[mask].flat,xi[mask].flat,aimg[mask].flat):
         print("({},{},{}): going to force pix0={}={} down to 3000".format(ix,iy,iz,pix0,img.GetPixel(ix,iy,iz)))
         img.SetPixel(ix,iy,iz,3000)
     mask=(aimg<-1024)
+    nunder=np.sum(mask.flat)
+    print("{} pixels have a value less than -1024".format(nunder))
     for ix,iy,iz,pix0 in zip(xi[mask].flat,yi[mask].flat,zi[mask].flat,aimg[mask].flat):
         print("({},{},{}): going to force pix0={} up to -1024".format(ix,iy,iz,pix0))
         img.SetPixel(ix,iy,iz,-1024)
+    return img
+
+if sitk.Version_MajorVersion() == 0 and sitk.Version_MinorVersion() <= 8:
+    print("WARNING: using manual clamping because your SimpleITK version {} ".format(sitk.Version_VersionString())
+          +"seems to be an old one (older than July 2015) with a bug in the Clamp function.")
+    sitk_clamp = old_sitk_clamp
+else:
+    print("YAY: using native sitk clamping function because " +
+          "your SimpleITK version seems to be new enough: {}.".format(sitk.Version_VersionString()))
+    sitk_clamp = new_sitk_clamp
+
+for mhd in mhdlist:
+    img=sitk.ReadImage(mhd)
+    cimg = sitk_clamp(img)
     cmhd=mhd.replace(".mhd",".clipped.mhd");
-    print("writing clamped file",cmhd);
-    sitk.WriteImage(img,cmhd)
+    sitk.WriteImage(cimg,cmhd)
