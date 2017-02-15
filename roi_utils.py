1a4500ff730a8e38999f1c4b3eebbe7ada223591
David Boersma <david.boersma@physics.uu.se>
2017-02-15 20:20:57 +0100

    make roi printable and get xyz right in debug info

--- roi_utils.py
+++ roi_utils.py
@@ -176,6 +176,9 @@ class region_of_interest(object):
             else:
                 logger.warn("{} not one single z step: {}".format(self.roiname,", ".join([str(d) for d in dz])))
                 self.dz = 0.
+    def __repr__(self):
+        return "roi {} defined by contours in {} layers, bounding box [[{},{}],[{},{}],[{},{}]]".format(
+                self.roiname,len(self.contour_layers), self.xmin, self.xmax, self.ymin, self.ymax, self.zmin, self.zmax)
     def have_mask(self):
         return self.dz != 0.
     def get_mask(self,img):
@@ -252,17 +255,24 @@ class region_of_interest(object):
         if debuglabel:
             print("START debugging "+debuglabel)
             amask0=amask*(aimg==0.0)
-            ix0,iy0,iz0 = np.where(amask0)
+            iz0,iy0,ix0 = np.where(amask0)
+            iz,iy,ix = np.where(amask)
             logger.debug('indices for zero dose voxels have {}/{}/{} different values (out of {}/{}/{})'.format(
                 len(set(ix0[:])), len(set(iy0[:])), len(set(iz0[:])),
                 len(ix0), len(iy0), len(iz0)))
-            assert((aimg[ix0,iy0,iz0]==0.0).all())
-            hix0,xedges=np.histogram(ix0,bins=np.arange(-0.5,aimg.shape[0]-0.49,1.0))
+            assert((aimg[iz0,iy0,ix0]==0.0).all())
+            hix0,xedges=np.histogram(ix0,bins=np.arange(-0.5,aimg.shape[2]-0.49,1.0))
             hiy0,yedges=np.histogram(iy0,bins=np.arange(-0.5,aimg.shape[1]-0.49,1.0))
-            hiz0,zedges=np.histogram(iz0,bins=np.arange(-0.5,aimg.shape[2]-0.49,1.0))
+            hiz0,zedges=np.histogram(iz0,bins=np.arange(-0.5,aimg.shape[0]-0.49,1.0))
             hixy0,foo,bar=np.histogram2d(ix0,iy0,bins=(xedges,yedges))
             hiyz0,foo,bar=np.histogram2d(iy0,iz0,bins=(yedges,zedges))
             hizx0,foo,bar=np.histogram2d(iz0,ix0,bins=(zedges,xedges))
+            rxmin=xedges[np.min(ix)]
+            rxmax=xedges[np.max(ix)+1]
+            rymin=yedges[np.min(iy)]
+            rymax=yedges[np.max(iy)+1]
+            rzmin=zedges[np.min(iz)]
+            rzmax=zedges[np.max(iz)+1]
             ahixy0 = np.array(hixy0).T
             ahiyz0 = np.array(hiyz0).T
             ahizx0 = np.array(hizx0).T
@@ -272,33 +282,45 @@ class region_of_interest(object):
             pylab.subplot(321)
             pylab.bar(xedges[:-1],hix0,lw=0,color='b')
             pylab.xlim(xedges[0],xedges[-1])
+            ymin,ymax=pylab.ylim()
+            pylab.plot([rxmin]*2,[ymin,ymax],'r-')
+            pylab.plot([rxmax]*2,[ymin,ymax],'r-')
             pylab.xlabel('X index')
             pylab.subplot(322)
             pylab.pcolormesh(yedges,zedges,ahiyz0)
-            #pylab.xlim(yedges[0],yedges[-1])
-            #pylab.ylim(zedges[0],zedges[-1])
+            pylab.plot([rymin,rymax,rymax,rymin,rymin],[rzmin,rzmin,rzmax,rzmax,rzmin],'y-')
+            pylab.xlim(yedges[0],yedges[-1])
+            pylab.ylim(zedges[0],zedges[-1])
             pylab.xlabel('Y index')
             pylab.ylabel('Z index')
             pylab.colorbar()
             pylab.subplot(323)
             pylab.bar(yedges[:-1],hiy0,lw=0,color='b')
+            ymin,ymax=pylab.ylim()
+            pylab.plot([rymin]*2,[ymin,ymax],'r-')
+            pylab.plot([rymax]*2,[ymin,ymax],'r-')
             pylab.xlabel('Y index')
             pylab.xlim(yedges[0],yedges[-1])
             pylab.subplot(324)
             pylab.pcolormesh(zedges,xedges,ahizx0)
-            #pylab.xlim(zedges[0],zedges[-1])
-            #pylab.ylim(xedges[0],xedges[-1])
+            pylab.plot([rzmin,rzmax,rzmax,rzmin,rzmin],[rxmin,rxmin,rxmax,rxmax,rxmin],'y-')
+            pylab.xlim(zedges[0],zedges[-1])
+            pylab.ylim(xedges[0],xedges[-1])
             pylab.xlabel('Z index')
             pylab.ylabel('X index')
             pylab.colorbar()
             pylab.subplot(325)
             pylab.bar(zedges[:-1],hiz0,lw=0,color='b')
             pylab.xlim(zedges[0],zedges[-1])
+            ymin,ymax=pylab.ylim()
+            pylab.plot([rzmin]*2,[ymin,ymax],'r-')
+            pylab.plot([rzmax]*2,[ymin,ymax],'r-')
             pylab.xlabel('Z index')
             pylab.subplot(326)
             pylab.pcolormesh(xedges,yedges,ahixy0)
-            #pylab.xlim(xedges[0],xedges[-1])
-            #pylab.ylim(yedges[0],yedges[-1])
+            pylab.plot([rxmin,rxmax,rxmax,rxmin,rxmin],[rymin,rymin,rymax,rymax,rymin],'y-')
+            pylab.xlim(xedges[0],xedges[-1])
+            pylab.ylim(yedges[0],yedges[-1])
             pylab.xlabel('X index')
             pylab.ylabel('Y index')
             pylab.colorbar()
@@ -329,5 +351,3 @@ class region_of_interest(object):
         else:
             logger.warn("dhistsum is zero or negative")
         return dvh, dedges, dhistsum, dsum
-                
-
