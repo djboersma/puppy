aca10b7ed6b6909ba8dfc364364145a6d4717cde
David Boersma <david.boersma@physics.uu.se>
2017-04-03 18:08:24 +0200

    add merging and intersect methods

--- bounding_box.py
+++ bounding_box.py
@@ -11,8 +11,7 @@ class bounding_box(object):
         nkeys = len(kwargs.keys())
         self.limits=np.empty((3,2))
         if nkeys == 0:
-            self.limits[:,0]=np.inf
-            self.limits[:,1]=-np.inf
+            self.reset()
         elif nkeys > 1:
             raise RuntimeError("too many arguments ({}) to bounding box constructor: {}".format(nkeys,kwargs))
         elif "bb" in kwargs:
@@ -27,6 +26,9 @@ class bounding_box(object):
                 raise ValueError("wrong shape for xyz limits: {}".format(self.limits.shape))
             if np.logical_not(self.limits[:,0]<=self.limits[:,1]).any():
                 raise ValueError("min should be less or equal max but I got min={} max={}".format(self.limits[:,0],self.limits[:,1]))
+    def reset(self):
+        self.limits[:,0]=np.inf
+        self.limits[:,1]=-np.inf
     def __repr__(self):
         return "bounding box [[{},{}],[{},{}],[{},{}]]".format(*(self.limits.flat[:].tolist()))
     def volume(self):
@@ -43,8 +45,8 @@ class bounding_box(object):
         apoint = np.array(point,dtype=float)
         assert(len(apoint.shape)==1)
         assert(apoint.shape[0]==3)
-        self.limits[:,0] = np.min([self.limits[:,0],point],axis=0)
-        self.limits[:,1] = np.max([self.limits[:,1],point],axis=0)
+        self.limits[:,0] = np.min([self.limits[:,0],apoint],axis=0)
+        self.limits[:,1] = np.max([self.limits[:,1],apoint],axis=0)
     def should_contain_all(self,points):
         assert(np.array(points).shape[1]==3)
         self.should_contain(np.min(points,axis=0))
@@ -61,6 +63,24 @@ class bounding_box(object):
             return ((point>=self.limits[:,0])*(point<=self.limits[:,1])).all()
     def encloses(self,bb,inner=False):
         return (self.contains(bb.mincorner(),inner) and self.contains(bb.maxcorner(),inner))
+    def add_margins(self,margins):
+        # works both with scalar and vector
+        self.limits[:,0]-=margins
+        self.limits[:,1]+=margins
+    def merge(self,bb):
+        if self.empty():
+            self.limits = np.copy(bb.limits)
+        elif not bb.empty():
+            self.should_contain(bb.mincorner())
+            self.should_contain(bb.maxcorner())
+    def have_overlap(self,bb):
+        return ((not self.empty()) and (not bb.empty()) and (self.limits[:,0]<=bb.limits[:,1]).all() and (bb.limits[:,0]<=self.limits[:,1]).all())
+    def intersect(self,bb):
+        if not self.have_overlap(bb):
+            self.reset()
+        else:
+            self.limits[:,0] = np.max([self.mincorner(),bb.mincorner()],axis=0)
+            self.limits[:,1] = np.min([self.maxcorner(),bb.maxcorner()],axis=0)
 
 #######################################################################
 # TESTING
@@ -145,3 +165,34 @@ class test_bounding_box(unittest.TestCase):
         bbxyz2 = bounding_box(xyz=[[1,2],[3,4],[5,6]])
         bbxyz2.should_contain_all( np.array(range(30)).reshape(10,3) )
         self.assertEqual(bbxyz,bbxyz2)
+    def test_grow_margin(self):
+        # scalar grow ##################################################
+        bbxyz = bounding_box(xyz=[1,2,3,4,5,6])
+        bbxyz.add_margins(0.5)
+        self.assertTrue( (bbxyz.limits.flat == np.array([0.5,2.5,2.5,4.5,4.5,6.5])).all() )
+        # scalar shrink ################################################
+        bbxyz = bounding_box(xyz=[1,2,3,4,5,6])
+        bbxyz.add_margins(-0.25)
+        self.assertTrue( (bbxyz.limits.flat == np.array([1.25,1.75,3.25,3.75,5.25,5.75])).all() )
+        # vector grow ##################################################
+        bbxyz = bounding_box(xyz=[1,2,3,4,5,6])
+        bbxyz.add_margins(np.array([1,3,5]))
+        self.assertTrue( (bbxyz.limits[:,0] == 0.).all() )
+        bbxyz = bounding_box(xyz=[1,2,3,4,5,6])
+        bbxyz.add_margins(np.array([8,6,4]))
+        self.assertTrue( (bbxyz.limits[:,1] == 10.).all() )
+    def test_merge(self):
+        bbxyz0 = bounding_box(xyz=np.arange(1,7))
+        bbxyz1 = bounding_box(xyz=np.array([1.1,1.9,3.1,3.9,5.1,5.9]))
+        bbxyz2 = bounding_box(xyz=np.arange(1.1,7,1.1))
+        bbxyz0.merge(bbxyz1) # should not change anything
+        self.assertTrue( (bbxyz0.limits[:,0] == np.arange(1,7,2)).all() )
+        bbxyz0.merge(bbxyz2)
+        self.assertTrue( (bbxyz0.limits[:,0] == np.arange(1,5.1,2)).all() ) # lower limits unchanged
+        self.assertTrue( (bbxyz0.limits[:,1] == np.arange(1.1,7,1.1)[1::2]).all() ) # upper limits changed
+    def test_intersect(self):
+        bbxyz0 = bounding_box(xyz=np.arange(1,7))
+        bbxyz1 = bounding_box(xyz=np.arange(1,7)+0.5)
+        bbxyz0.intersect(bbxyz1)
+        self.assertTrue( (bbxyz0.limits[:,0] == np.arange(1.5,5.6,2)).all() ) # lower limits changed
+        self.assertTrue( (bbxyz0.limits[:,1] == np.arange(2,7,2)).all() ) # upper limits unchanged
