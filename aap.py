c33dee82c5f672d3f3bb00e9e414056b6bbac89a
David Boersma <david.boersma@physics.uu.se>
2017-04-04 18:15:47 +0200

    noot mies

--- aap.py
+++ aap.py
@@ -4,14 +4,25 @@ import numpy as np
 import os,sys
 from roi_utils import region_of_interest, get_intersection_volume, list_roinames
 import dicom
+import logging
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
 
-ss = dicom.read_file(os.path.join(os.environ['DICOM_DIRECTORY'],'DICOM_4D_STUDIE','44','STRUCTURESET.dcm'))
+logger.info("PATIENT 44")
+ss44 = dicom.read_file(os.path.join(os.environ['DICOM_DIRECTORY'],'DICOM_4D_STUDIE','44','STRUCTURESET.dcm'))
+#print("\n".join(list_roinames(ss)))
+itv44 = region_of_interest(ss44,'ITV 0+20+50+80+M')
+ctv44 = region_of_interest(ss44,'CTV 50')
+logger.info('itv volume is {}'.format(itv44.get_volume()))
+logger.info('ctv volume is {}'.format(ctv44.get_volume()))
+logger.info('itv ctv intersection volume is {}'.format(get_intersection_volume([itv44,ctv44])))
 
-print("\n".join(list_roinames(ss)))
+logger.info("PATIENT 51")
+ss51 = dicom.read_file(os.path.join(os.environ['DICOM_DIRECTORY'],'DICOM_4D_STUDIE','51_P1','RS.Test_C_hodgkin-051.dcm'))
+ctv51 = region_of_interest(ss51,'4D CTV')
+logger.info('ctv volume is {}'.format(ctv51.get_volume()))
 
-itv = region_of_interest(ss,'ITV 0+20+50+80+M')
-ctv = region_of_interest(ss,'CTV 50')
-
-print('itv volume is {}'.format(itv.get_volume()))
-print('ctv volume is {}'.format(ctv.get_volume()))
-print('itv ctv intersection volume is {}'.format(get_intersection_volume([itv,ctv])))
+logger.info("PATIENT 53")
+ss53 = dicom.read_file(os.path.join(os.environ['DICOM_DIRECTORY'],'DICOM_4D_STUDIE','53_SHORT','STRUCTURESET.dcm'))
+ctv53 = region_of_interest(ss51,'CTV50')
+logger.info('ctv volume is {}'.format(ctv53.get_volume()))
