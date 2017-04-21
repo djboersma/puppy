6c6e6bc00092717d3c58f8ee2d76f1962db938ee
David Boersma <david.boersma@physics.uu.se>
2017-04-21 12:07:26 +0200

    more tests, some docs to the tests

--- mhd_zpad.py
+++ mhd_zpad.py
@@ -59,12 +59,26 @@ if __name__ == '__main__':
 import unittest
 
 class test_zpad(unittest.TestCase):
+    """
+        What are we testing today:
+        - zpad throws an exception unless
+            - first arg is a SimpleITK image
+            - SimpleITK image is 3D
+            - SimpleITK image voxel values are scalar
+            - second and third arg are int
+            - second and third arg are not negative
+            - at least one of second and third arg must be positive
+        - zpad will add any number of layers (copies of bottom layer) to the bottom
+        - zpad will add any number of layers (copies of top layer) to the top
+        - zpad will both of those in one call
+        - zpad will never change the input image
+    """
     def setUp(self):
         self.a2 = np.random.randint(-1024,high=4096,size=(3,4))
         self.img2 = sitk.GetImageFromArray(self.a2)
         self.a3 = np.random.randint(-1024,high=4096,size=(3,4,5))
         #print("3d image has {} voxels with value zero".format(np.sum(self.a3==0)))
-        self.img3 = sitk.GetImageFromArray(self.a3)
+        self.img3 = sitk.GetImageFromArray(self.a3.swapaxes(0,2))
         self.a4 = np.random.randint(-1024,high=4096,size=(3,4,5,6))
         self.img4 = sitk.GetImageFromArray(self.a4)
     def test_wrong_types(self):
@@ -79,43 +93,47 @@ class test_zpad(unittest.TestCase):
         with self.assertRaises(ValueError):
             zpad(self.img3,0,0); # at least one padding argument should be larger than 0
         with self.assertRaises(ValueError):
-            zpad(self.img3,-5,10); # both ints should be positive
+            zpad(self.img3,-5,10); # both ints should be nonnegative
         with self.assertRaises(ValueError):
-            zpad(self.img3,10,-5); # both ints should be positive
+            zpad(self.img3,10,-5); # both ints should be nonnegative
         with self.assertRaises(ValueError):
             zpad(self.img2,1,2); # image should be 3D
         with self.assertRaises(TypeError):
             zpad(self.img4,1,2); # image should be 3D and have scalar voxel type
     def test_bottom(self):
+        #print("bottom layer is {}".format(self.a3[:,:,0]))
         for p in [1,5,15]:
             img3 = sitk.Image(self.img3)
             img3.MakeUnique()
             size3 = img3.GetSize()
             padded_img3 = zpad(img3,p,0)
+            bi3 = sitk.GetArrayFromImage(img3).swapaxes(0,2)
+            self.assertTrue((bi3 == self.a3).all()) # check that zpad did not alter the input image
             psize3 = padded_img3.GetSize()
             self.assertEqual(size3[0],psize3[0])
             self.assertEqual(size3[1],psize3[1])
             self.assertEqual(size3[2]+p,psize3[2])
-            ai3 = sitk.GetArrayFromImage(img3).swapaxes(0,2)
             api3 = sitk.GetArrayFromImage(padded_img3).swapaxes(0,2)
             for iz in range(p):
                 #print("testing bottom iz={} for p={}".format(iz,p))
-                self.assertTrue((api3[:,:,iz]==ai3[:,:,0]).all())
+                self.assertTrue((api3[:,:,iz]==self.a3[:,:,0]).all())
     def test_top(self):
+        #print("top layer is {}".format(self.a3[:,:,-1]))
         for q in [1,5,15]:
             img3 = sitk.Image(self.img3)
             img3.MakeUnique()
             size3 = img3.GetSize()
             padded_img3 = zpad(img3,0,q)
+            bi3 = sitk.GetArrayFromImage(img3).swapaxes(0,2)
+            self.assertTrue((bi3 == self.a3).all()) # check that zpad did not alter the input image
             psize3 = padded_img3.GetSize()
             self.assertEqual(size3[0],psize3[0])
             self.assertEqual(size3[1],psize3[1])
             self.assertEqual(size3[2]+q,psize3[2])
-            ai3 = sitk.GetArrayFromImage(img3).swapaxes(0,2)
             api3 = sitk.GetArrayFromImage(padded_img3).swapaxes(0,2)
             for iz in range(q):
                 #print("testing top iz={} for q={}".format(iz,q))
-                self.assertTrue((api3[:,:,-iz-1]==ai3[:,:,-1]).all())
+                self.assertTrue((api3[:,:,-iz-1]==self.a3[:,:,-1]).all())
     def test_both(self):
         for p in [0,1,5,15]:
             for q in [0,4,16]:
@@ -125,15 +143,16 @@ class test_zpad(unittest.TestCase):
                 img3.MakeUnique()
                 size3 = img3.GetSize()
                 padded_img3 = zpad(img3,p,q)
+                bi3 = sitk.GetArrayFromImage(img3).swapaxes(0,2)
+                self.assertTrue((bi3 == self.a3).all()) # check that zpad did not alter the input image
                 psize3 = padded_img3.GetSize()
                 self.assertEqual(size3[0],psize3[0])
                 self.assertEqual(size3[1],psize3[1])
                 self.assertEqual(size3[2]+p+q,psize3[2])
-                ai3 = sitk.GetArrayFromImage(img3).swapaxes(0,2)
                 api3 = sitk.GetArrayFromImage(padded_img3).swapaxes(0,2)
                 for iz in range(p):
                     #print("both: testing bottom iz={} for p={}".format(iz,q))
-                    self.assertTrue((api3[:,:,iz]==ai3[:,:,0]).all())
+                    self.assertTrue((api3[:,:,iz]==self.a3[:,:,0]).all())
                 for iz in range(q):
                     #print("both: testing top iz={} for q={}".format(iz,q))
-                    self.assertTrue((api3[:,:,-iz-1]==ai3[:,:,-1]).all())
+                    self.assertTrue((api3[:,:,-iz-1]==self.a3[:,:,-1]).all())
